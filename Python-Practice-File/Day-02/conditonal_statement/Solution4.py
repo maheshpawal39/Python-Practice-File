36c25f6b8db4590a75bed570806dcb5684b1548c
Mahesh Pawal <maheshpawal39@gmail.com>
2026-08-03 12:48:18 +0530

    Add files via upload

---
+++ Python-Practice-File/Day-02/conditonal_statement/Solution4.py
@@ -0,0 +1,36 @@
+fruit = input("Enter the value of your fruit name : ")
+color = input("Enter the value of your fruit color : ")
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Fruit is unripe")
+    elif color == "Yellow":
+        print("Fruit is ripe")    
+    elif color == "Brown":
+        print("Fruit is overripe")
+    else :
+        print("plz enter verify fruit") 
+    exit()
+
+if fruit == "Orange":
+    if color == "Green":
+        print("Fruit is unripe")
+    elif color == "Yellow":
+        print("Fruit is ripe")    
+    elif color == "Brown":
+        print("Fruit is overripe")
+    else :
+        print("plz enter verify fruit") 
+    exit()
+
+
+if fruit == "lichi":
+    if color == "Green":
+        print("Fruit is unripe")
+    elif color == "Yellow":
+        print("Fruit is ripe")    
+    elif color == "Brown":
+        print("Fruit is overripe")
+    else :
+        print("plz enter verify fruit")  
+    exit()
