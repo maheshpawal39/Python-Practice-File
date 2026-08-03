d4ad1f811376be1410570d0036564794fc5610b7
Mahesh Pawal <maheshpawal39@gamil.com>
2026-08-03 15:53:52 +0530

    Added Solution10

---
+++ Python-Practice-File/Day-02/conditonal_statement/Solution9.py
@@ -0,0 +1,6 @@
+year = int(input("Enter Year : "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0 ):
+    print(year , "is a leap year")
+else:
+    print(year , "not a leap year")    
