cdfb4e9ebf6129dd09d5ba0212b9a4136ad2d6e4
Mahesh Pawal <maheshpawal39@gmail.com>
2026-08-03 12:50:41 +0530

    Delete Day-02/conditonal_statement directory

--- Day-02/conditonal_statement/Solution7.py
+++
@@ -1,9 +0,0 @@
-ord_size = "Medium"
-extra_short = True
-
-if extra_short:
-    coffee = ord_size + " coffee with an extra short"
-else:
-   coffee =  ord_size + "coffee"
-
-print("order :" , coffee)   
--- Day-02/conditonal_statement/Solution8.py
+++
@@ -1,8 +0,0 @@
-password = input("Enter yoyr password length: ")
-
-if len(password) < 6:
-    print("password is week")
-elif len(password) <=10 :
-    print("password is Medium")
-else:
-    print("password is strong")    
